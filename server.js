73a395b1d557d3f3eb9d4e64fbf4da890931396e
shymaSane <shymaSane1@gmail.com>
2019-02-02 18:04:14 +0200

    connect mongoose with m-lab

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const mongoose  = require('mongoose')
 const app = express()
 const port = process.env.PORT || 5000 
 
+//@routes
 const users = require('./routers/users')
 const profiles = require('./routers/profiles')
 const stories = require('./routers/stories')
+
+//@connect monogo
+mongoose
+    .connect('mongodb://connect:shymaSane1@ds159634.mlab.com:59634/connect',  { useNewUrlParser: true })
+    .then(() => console.log('mongoose connected'))
+    .catch((err) => console.log('mongoose didnt connect err:' + err))
 
 app.get('/', (req, res) => {
     res.send('working')
 })
 
+//@using routes
 app.use('/api/users', users)
 app.use('/api/profiles', profiles)
 app.use('/api/stories', stories)
