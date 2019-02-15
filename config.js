69436e9155550be331cade54d28de5468789292b
shymaSane <shymaSane1@gmail.com>
2019-02-15 16:06:39 +0200

    add: hashing function to user model

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     development:{
-        port: process.env.port || 5000
+        port: process.env.port || 5000,
+        saltingRounds: 10
     }
 }
--- models/users.js
+++ models/users.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const  bcrypt = require('bcrypt');
+
+const environment = process.env.NODE_ENV;
+const stage = require('./config')[environment];
+
+//schema maps to a collection
 const Schema = mongoose.Schema;
 
 //user schema 
@@ -24,5 +30,28 @@ const UserSchema = new Schema({
         unique: true
     }
 })
+
+
+//encrypt password before save:
+UserSchema.pre('save', (next) => {
+    //current user schema
+    const user = this
+    //make sure t not hash is the use isnt new
+    if(!user.isModified || !user.isNew){
+        next()
+    } else {
+        bcrypt.hash(user.password, stage.saltingRounds, (err, hash) => {
+            if(err){
+                console.log(`error in hashing password for user:${user.name}`)
+                next(err)
+            } else {
+                user.password = hash
+                next()
+            }
+        })
+    }
+    
+})
+
 
 module.exports = mongoose.model('User', UserSchema)
