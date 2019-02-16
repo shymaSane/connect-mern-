e5802cc0097133b8d00e108a1a204cc3f9b4aa81
shymaSane <shymaSane1@gmail.com>
2019-02-16 18:56:35 +0200

    add: add func to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,36 @@
 const mongoose = require('mongoose');
+const User = require('../models/users')
+
 
 
 module.exports={
     add: (req,res) =>{
-        res.send('hello')
+        mongoose
+        .connect(process.env.DB_URI,  { useCreateIndex: true, useNewUrlParser: true })
+        .then(() => {
+            let result = {};
+            let status = 201
+            const {name, email, password} = req.body
+            //create new document and save user information in it
+            const user = new User({name, email, password})
+            //save document 
+            user.save()
+            .then((user) => {
+                result.status = status
+                result.value = user
+            })
+            .catch((err) => {
+                status = 500
+                result.status = status
+                result.error = err
+            })
+            res.status(status).send(result)
+        })
+        .catch((err) => {
+            status = 500
+            result.status = status
+            result.error = err
+            res.status(status).send(result)
+        })
     }
 }
