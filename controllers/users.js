cf4d04eb76a2b50c7d647f30e10d147900b41f0d
shymaSane <shymaSane1@gmail.com>
2019-02-21 19:46:45 +0200

    finished: login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
 const User = require('../models/users')
+const bcrypt = require('bcrypt')
 
 
 
@@ -10,7 +11,7 @@ module.exports={
         .connect(process.env.DB_URI,  { useCreateIndex: true, useNewUrlParser: true })
         .then(() => {
             let result = {};
-            let status = 201
+            let status = 201; //created
             const {name, email, password} = req.body
             //create new document and save user information in it
             const user = new User({name, email, password})
@@ -36,25 +37,64 @@ module.exports={
     },
     login: (req, res) => {
         let result = {}
-        let status = 202; //accepted
-        //fetch user
+        let status = 200; 
+        //connect to mongoose
         mongoose
         .connect(process.env.DB_URI,  { useCreateIndex: true, useNewUrlParser: true })
         .then(() => {
             //contains email and passwrod
             const {email, password} = req.body
-            result.value = {email, password};
-            result.status; //accepted
+            //fetch user from DB
             User.findOne({email})
             .then((user) => {
-                //if the user is there do sth
+                //compare passwrods with the one in the db 
+                bcrypt.compare(password, user.password)
+                    .then((res) => {
+                        //if password is matched then res = true
+                        if(res){
+                            const payload = {user: user.email}
+                            const options = {expiresIn: '2d'}
+                            const secret = process.env.JWT_SECRET
+                            const token = jwt.sign(payload, options, secret)
+
+                            //result
+                            result.value = user;
+                            result.token = token;
+                            result.status = status
+                        } else {
+                            //no match either password wrong or email
+                            status = 401
+                            result.erorr = "invalid password or email"
+                            result.status = status; //unauthorized
+                        }
+                        res.status(status).send(result)
+                })
+                .catch((err) =>{
+                    //bcrypt error
+                    status = 500;
+                    result.erorr = err;
+                    result.status = status;
+
+                    res.status(status).send(result)
+                })
+
             })
             .catch((err) => {
-                //if the user didnt singup 
+                //db error 
+                status = 404; //it may be avaliable with refresh
+                result.erorr = err;
+                result.status = status;
+
+                res.status(status).send(result)
             })
         })
         .catch((err) => {
-            
+            //data base connection error
+            status = 500;
+            result.erorr = err;
+            result.status = status;
+
+            res.status(status).send(result)
         })
     }
 }
