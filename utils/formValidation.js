4018280ad5578e2d160dcc4b60d5faf6f3c374a6
shymaSane <shymaSane1@gmail.com>
2019-03-04 14:47:28 +0200

    fix: signup validation

--- utils/formValidation.js
+++ utils/formValidation.js
@@ -1,12 +1,12 @@
 const validator =  require('validator');
 
-const isEmpty = (value) =>
-    value === null ||
+const isEmpty = (value) => {
+   return value === null ||
     value === undefined ||
-    (typeof(value) === object && Object.keys(value).lenght === 0) ||
-    (typeof(value) === string && value.trim().length === 0)
+    (typeof(value) === 'object' && Object.keys(value).lenght === 0) ||
+    (typeof(value) === 'string' && value.trim().length === 0)}
 
-exports.signUpValidation = (req, res, next) => {
+const signUpValidation = (req, res, next) => {
     const{name, email, password, password2} = req.body 
     let erorrs = {};
     //ckeck is email valid
@@ -17,13 +17,13 @@ exports.signUpValidation = (req, res, next) => {
     if(!validator.isLength(name, {min: 4, max: 20})){
         erorrs.name = "name must be between 4 and 20 characters"
     }
-    //check if password length between 4 and 20
-    if(!validator.isLength(password, {min: 4, max: 20})){
-        erorrs.password = "password must be between 4 and 20 characters"
-    }
     //check if passwords matching
     if(!validator.equals(password, password2)){
         erorrs.password = "passwords are not matching, please enter your password again"
+    }
+    //check if password length between 4 and 20
+    if(!validator.isLength(password, {min: 4, max: 20})){
+        erorrs.password = "password must be between 4 and 20 characters"
     }
     //check if fields are empty
     if(isEmpty(name)){
@@ -42,3 +42,5 @@ exports.signUpValidation = (req, res, next) => {
         res.status(400).send(erorrs)
     }
 }
+
+module.exports = {signUpValidation}
