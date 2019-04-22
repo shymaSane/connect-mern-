4111eb597090025e25f543c39711001b2e4d782b
shymaSane <shymaSane1@gmail.com>
2019-04-22 13:00:35 +0300

    add: finished get profile by user name

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,40 +2,34 @@ const User = require('../models/User')
 const Profile = require('../models/Profile')
 
 module.exports = {
-    getCurrentUserProfile: (req, res) =>{
-        //TODO: check if user is the owner f the profile or visitor 
+    getProfile: (req, res) =>{
         //take user_name from token 
         const tokenPayload = JSON.stringify(req.decoded)
-        const name = JSON.parse(tokenPayload).user
+        const user_name = JSON.parse(tokenPayload).user_name
         let result = {};
         let status;
-        //find user in DB
-        User.findOne({name})
-            .then((user) =>{
-                if(user){
-                    // status = 200;
-                    // result.status = status;
-                    // result.value = user
-                    // res.status(status).send(result) 
-                    
-                    //check if there is profile in db or not
-
-                }else{
-                    //there is no user
-                    status = 409;//conflict
-                    result.erorr = "user not found";
-                    result.status = status;
-                    res.status(status).send(result)
+        //if user_name params is equal to user_name from token then enable edit and delete
+        Profile.findOne({handle: req.params.username})
+            .then((profile)=> {
+                if(profile.handle == user_name){
+                    status = 200;
+                    result.value = profile
+                    result.properties = `editable and deletable`
+                } else {
+                    status = 200;
+                    result.value = profile
+                    result.properties = `no edit or delete`
                 }
-            })
-            .catch((err) =>{
-                //interal eror
-                status = 500;//conflict
-                result.erorr = err;
-                result.status = status;
                 res.status(status).send(result)
             })
-
+            .catch((err) => {
+                status = 500;
+                result.error = err
+                res.status(status).send(result)
+            })
+        // if(user_name == req.params.username){
+            
+        // }
     },
     editProfile: () => { 
 
