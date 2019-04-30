0c6474e7f2fb640b6548d8b55f889d5684e26ac5
shymaSane <shymaSane1@gmail.com>
2019-04-30 15:01:39 +0300

    add: story intial schema design

--- models/Story.js
+++ models/Story.js
@@ -1,6 +1,68 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
+//story itself/comments/likes 
 const StorySchema = new Schema({
-    
+    //story has:
+    //user_id
+    user_id:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    //title
+    title: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    //body
+    body: {
+        type: String,
+        required: true,
+    },
+    //genere(CSV)
+    genere: {
+        type: [String],
+        required: true
+    },
+    //tags(romantic, fanfiction, ...etc)
+    tags: {
+        type: [String],
+        required: true
+    },
+    //likes(user_id)
+    likes: [
+        {
+            user_id: {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        }
+    ],
+    //comments(user_is, text)
+    comments: [
+        {
+            user_id: {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            },
+            text: {
+                type: String,
+                required: true,
+                maxlength: 5000
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    //date
+    date: {
+        type: Date,
+        default: Date.now
+    }
+    //TODO: Link parts together
 })
+
+module.exports = mongoose.model('Story', StorySchema)
