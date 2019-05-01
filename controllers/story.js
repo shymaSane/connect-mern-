b3521a7e8ec704b5de6b35bc23dc7e3e96706ede
shymaSane <shymaSane1@gmail.com>
2019-05-01 13:59:10 +0300

    add: get story controller

--- controllers/story.js
+++ controllers/story.js
@@ -4,9 +4,29 @@ const User = require('../models/User')
 module.exports = {
     getStory: (req, res) => {
         //TODO: add delete and edit button for owners
+        const story_id = req.params.id;
+        let result = {};
+        let status = 200;
+        Story.findOne({_id: story_id})
+        .exec()
+        .then((story) => {
+            if(story){
+                result.value = story;
+                status = 200
+                res.status(status).send(result)
+            } else {
+                result.error = "Not found"
+                status = 404
+                res.status(status).send(result)
+            }
+        })
+        .catch((err) => {
+            res.status(500).send(err)
+        })
     },
     addStory: (req, res) => {
         //TODO: add new stry to db
+        //need validation
     },
     getEditStory: (req, res) => {
         //TODO: fetch exists story
