8eef194135d412509eadb6ed4c97a881be20df2e
shymaSane <shymaSane1@gmail.com>
2019-06-23 14:11:12 +0300

    add: redux and react-router dependencies

---
+++ client/src/components/registration/login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>sign in page</h1>
+            </div>
+        )
+    }
+}
+
+export default login;
