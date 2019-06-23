381d425defb908c8422b897fc86c90e36e55edbf
shymaSane <shymaSane1@gmail.com>
2019-06-23 15:32:51 +0300

    add: signup component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Signup from './components/registration/signup/Signup'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello its me shyma </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello its me shyma </h1>
+        <Link to='/api/user/signup'>signup</Link>
+          <Switch>
+            <Route exact path = '/api/user/signup' component={Signup}></Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
